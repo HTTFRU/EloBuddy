98d8ba57c15c37ecd45c97b702b5c23ffa7dff03
HTTFRU <skills1dll@gmail.com>
2016-11-17 16:03:43 +0300

    release alfa YASUO

---
+++ [HTTF] Yasuo/HTTF Yasuo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+
+
+namespace HTTF_Yasuo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != "Yasuo")
+                return;
+
+            YasuoMenu.Load();
+            Utils.ForDash.Init();
+
+
+        }
+    }
+}
---
+++ [HTTF] Yasuo/HTTF Yasuo/Utils/ForSpell.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using SharpDX;
+
+namespace HTTF_Yasuo.Utils
+{
+    static class ForSpell
+    {
+        public static bool HasWhirlwind(this AIHeroClient unit)
+        {
+            return unit.HasBuff("YasuoQ3W");
+        }
+
+        public static bool IsKnockedUp(this Obj_AI_Base unit)
+        {
+            return unit.HasBuffOfType(BuffType.Knockback) || unit.HasBuffOfType(BuffType.Knockup);
+        }
+
+        public static bool IsUnderTower(this Vector3 v)
+        {
+            return EntityManager.Turrets.Enemies.Where(a => a.Health > 0 && !a.IsDead).Any(a => a.Distance(v) < 950);
+        }
+        
+        public static bool CanDash(this Obj_AI_Base unit)
+        {
+            return !unit.HasBuff("YasuoDashWrapper");
+        }
+    }
+}
